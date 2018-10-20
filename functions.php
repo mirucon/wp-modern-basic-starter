2a0f11f27c45898b2c5f206b80aed63fb1348b00
Toshihiro Kanai <i@miruc.co>
2018-10-20 16:41:56 +0900

    style: Conforms to WPCS 1.1

--- functions.php
+++ functions.php
@@ -11,9 +11,21 @@
  * @since 0.1.0
  **/
 add_action(
-	'wp_enqueue_scripts', function() {
-		wp_enqueue_style( 'theme-style', get_theme_file_uri( 'public/css/style.css' ), [], '0.1.0' );
-		wp_enqueue_script( 'theme-script', get_theme_file_uri( 'public/js/script.js' ), [], '0.1.0', false );
+	'wp_enqueue_scripts',
+	function () {
+		wp_enqueue_style(
+			'theme-style',
+			get_theme_file_uri( 'public/css/style.css' ),
+			[],
+			'0.1.0'
+		);
+		wp_enqueue_script(
+			'theme-script',
+			get_theme_file_uri( 'public/js/script.js' ),
+			[],
+			'0.1.0',
+			false
+		);
 	}
 );
 
@@ -23,18 +35,23 @@ add_action(
  *
  * @since 0.1.0
  *
- * @param string $tag Tag name.
+ * @param string $tag    Tag name.
  * @param string $handle Handle name.
+ *
  * @return string
  */
 add_filter(
-	'script_loader_tag', function( $tag, $handle ) {
+	'script_loader_tag',
+	function ( $tag, $handle ) {
 		$scripts_to_defer = [ 'theme-script' ];
 		foreach ( $scripts_to_defer as $defer_script ) {
 			if ( $defer_script === $handle ) {
 				return str_replace( ' src', ' defer src', $tag );
 			}
 		}
+
 		return $tag;
-	}, 10, 2
+	},
+	10,
+	2
 );
